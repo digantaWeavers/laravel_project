5c3c2479220718c0f06d17105df898d5a9da691e
mrdigantamaity24 <maitydiganta2017@gmail.com>
2024-08-07 13:10:50 +0530

    init

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<!----------- FOOTER start ----------->
+<footer>
+    <p>© Copyright 2018 Mobirise - All Rights Reserved</p>
+</footer>
+<!----------- FOOTER end ----------->
+</body>
+
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Caveat:400,700&subset=cyrillic" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/4326f6cda5.js"></script>
+    <link rel="stylesheet" href="{{ asset('custom.css') }}">
+    <title>CSS - Final Project</title>
+</head>
+
+<body>
+    <!----------- HEADER start ----------->
+    <header>
+        <nav>
+            <img src="https://mobirise.com/extensions/organicamp/assets/images/logo1.jpg" id="logo"
+                alt="Main logo">
+            <div>
+                <ul id="top-menu">
+                    <li id="drop-menu" class="menu-item">
+                        <a href="#">Demo Pages<i class="fas fa-caret-down"></i></a>
+                        <ul id="dropdown-menu">
+                            <li><a href="#">Menu Item 1</a></li>
+                            <li><a href="#">Menu Item 1</a></li>
+                            <li><a href="#">Menu Item 1</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="menu-item"><a href="#">Watch Video</a></li>
+                    <li class="menu-item"><a href="#">Blocks<i class="fas fa-caret-down"></i></a></li>
+                    <li><a href="#">Buy now</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div id="header-desc">
+            <h1>Vegan Store Simple Website Template</h1>
+            <p>With nutrition, all the vital functions of the body are connected. It is the source of the development of
+                tissues and cells, their constant renewal, the saturation of man with energy.</p>
+        </div>
+    </header>
+    <!----------- HEADER end ----------->
