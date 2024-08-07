8c2c184e282a0757bf4f65fd284aa75b197e4d3c
digantaWeavers <diganta.maity@weavers-web.com>
2024-08-07 20:07:43 +0530

    update

--- resources/views/SuperAdmin/dashboard.blade.php
+++ resources/views/SuperAdmin/dashboard.blade.php
@@ -1,1 +1,119 @@
-<h1>This Is Super Admin {{ Auth::user()->id }}</h1>
+@include('SuperAdmin/header')
+@php
+    $user = Auth::user();
+@endphp
+<main class="main-wrapper col-md-9 ms-sm-auto py-4 col-lg-9 px-md-4 border-start">
+    <div class="title-group mb-3">
+        <h1 class="h2 mb-0">Overview</h1>
+
+        <small class="text-muted">Hello {{ $user->fullname }}, welcome back!</small>
+    </div>
+
+    <div class="row my-4">
+        <div class="col-lg-7 col-12">
+            <div class="row mb-2">
+                <div class="col-lg-6">
+                    <div class="card custom-block bg-white">
+                        <div class="card-body">
+                            <h5 class="card-title">Total Projects</h5>
+                            <p class="card-text">10</p>
+                            <a href="" class="btn btn-info"><i class="bi-plus"></i> Add Project</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="card custom-block bg-white">
+                        <div class="card-body">
+                            <h5 class="card-title">Total Managers</h5>
+                            <p class="card-text">20</p>
+                            <a href="" class="btn btn-info"><i class="bi-plus"></i> Add Manager</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-2">
+                <div class="col-lg-6">
+                    <div class="card custom-block bg-white">
+                        <div class="card-body">
+                            <h5 class="card-title">Total TeamLeads</h5>
+                            <p class="card-text">10</p>
+                            <a href="" class="btn btn-info"><i class="bi-plus"></i> Add Lead</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="card custom-block bg-white">
+                        <div class="card-body">
+                            <h5 class="card-title">Total Employees</h5>
+                            <p class="card-text">20</p>
+                            <a href="" class="btn btn-info"><i class="bi-plus"></i> Add Employee</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-5 col-12">
+            <div class="custom-block custom-block-profile-front custom-block-profile text-center bg-white">
+                <div class="custom-block-profile-image-wrap mb-4">
+                    @if (empty($user->profilepic))
+                        <img src="https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659652_640.png"
+                            class="profile-image img-fluid" alt="Dummy Image">
+                    @else
+                        <img src="{{ asset('/storage/' . $user->product_image) }}" class="profile-image img-fluid"
+                            alt="Profile Picture">
+                    @endif
+                    <a href="setting.html" class="bi-pencil-square custom-block-edit-icon"></a>
+                </div>
+
+                <p class="d-flex flex-wrap mb-2">
+                    <strong>Name:</strong>
+
+                    <span>{{ $user->fullname }}</span>
+                </p>
+
+                <p class="d-flex flex-wrap mb-2">
+                    <strong>Username:</strong>
+
+                    <span>{{ $user->username }}</span>
+                </p>
+
+                <p class="d-flex flex-wrap mb-2">
+                    <strong>Email:</strong>
+
+                    <a href="mailto:{{ $user->emailaddress }}">
+                        {{ $user->emailaddress }}
+                    </a>
+                </p>
+
+                <p class="d-flex flex-wrap mb-0">
+                    <strong>Phone:</strong>
+
+                    <a href="tel:{{ $user->mobileno }}">
+                        {{ $user->mobileno }}
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-12 col-12">
+                    <p class="copyright-text">Copyright © Mini Finance 2048
+                        - Design: <a rel="sponsored" href="https://www.tooplate.com" target="_blank">Tooplate</a></p>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+</main>
+
+</div>
+</div>
+@include('SuperAdmin/footer')
---
+++ resources/views/SuperAdmin/header.blade.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="Tooplate">
+
+    <title>Mini Finance Dashboard Template</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400;700&display=swap" rel="stylesheet">
+
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/apexcharts.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/tooplate-mini-finance.css') }}" rel="stylesheet">
+</head>
+
+@php
+    $user = Auth::user();
+@endphp
+
+<body>
+    <header class="navbar sticky-top flex-md-nowrap">
+        <div class="col-md-3 col-lg-3 me-0 px-3 fs-6">
+            <a class="navbar-brand" href="index.html">
+                <i class="bi-box"></i>
+                SuperAdmin
+            </a>
+        </div>
+
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-nav me-lg-2">
+            <div class="nav-item text-nowrap d-flex align-items-center">
+
+                <div class="dropdown px-3">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        @if (empty($user->profilepic))
+                            <img src="https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659652_640.png" class="profile-image img-fluid" alt="Dummy Image">
+                        @else
+                            <img src="{{ asset('/storage/' . $user->product_image) }}" class="profile-image img-fluid" alt="Profile Picture">
+                        @endif
+                    </a>
+                    <ul class="dropdown-menu bg-white shadow">
+                        <li>
+                            <div class="dropdown-menu-profile-thumb d-flex">
+                                @if (empty($user->profilepic))
+                                    <img src="https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659652_640.png" class="profile-image img-fluid" alt="Dummy Image">
+                                @else
+                                    <img src="{{ asset('/storage/' . $user->product_image) }}" class="profile-image img-fluid" alt="Profile Picture">
+                                @endif
+
+                                <div class="d-flex flex-column">
+                                    <small>{{ $user->fullname }}</small>
+                                    <a href="#">{{ $user->emailaddress }}</a>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a class="dropdown-item" href="profile.html">
+                                <i class="bi-person me-2"></i>
+                                Profile
+                            </a>
+                        </li>
+
+                        <li>
+                            <a class="dropdown-item" href="setting.html">
+                                <i class="bi-gear me-2"></i>
+                                Settings
+                            </a>
+                        </li>
+
+                        <li class="border-top mt-3 pt-2 mx-4">
+                            <a class="dropdown-item ms-0 me-0" href="{{ route('superadmin.logout') }}">
+                                <i class="bi-box-arrow-left me-2"></i>
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-3 d-md-block sidebar collapse">
+                <div class="position-sticky py-4 px-3 sidebar-sticky">
+                    <ul class="nav flex-column h-100">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.html">
+                                <i class="bi-house-fill me-2"></i>
+                                Overview
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="profile.html">
+                                <i class="bi-person me-2"></i>
+                                Profile
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="setting.html">
+                                <i class="bi-gear me-2"></i>
+                                Settings
+                            </a>
+                        </li>
+
+                        <li class="nav-item border-top mt-auto pt-2">
+                            <a class="nav-link" href="{{ route('superadmin.logout') }}">
+                                <i class="bi-box-arrow-left me-2"></i>
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
